62334439a0ccf4c8b59b2ca358dd12ca87d84754
MinJae Lee <wjdchs0129@gnu.ac.kr>
2024-09-07 18:27:09 +0900

    유니티 통신을 위한 UDP sender/receiver 개발

--- python/poseEstimation/unityCommunication.py
+++ python/poseEstimation/unityCommunication.py
@@ -1,19 +1,16 @@
-# unityCommunication.py
 import socket
 import json
 
 
-def send_pose_to_unity(pose_data):
+def send_pose_to_unity(data):
     try:
-        # Setup UDP connection to Unity
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         unity_address = (
             "127.0.0.1",
             5005,
-        )  # Replace with your Unity server IP and port
+        )
 
-        # Convert pose data to JSON
-        message = json.dumps(pose_data)
+        message = json.dumps(data)
         sock.sendto(message.encode("utf-8"), unity_address)
 
         sock.close()
@@ -21,3 +18,22 @@ def send_pose_to_unity(pose_data):
     except Exception as e:
         print(f"Network error: {e}")
         return False
+
+
+if __name__ == "__main__":
+    import socket
+
+    # 서버 설정
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = 5005
+
+    # 소켓 생성
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+
+    print(f"UDP 서버가 {UDP_IP}:{UDP_PORT} 에서 리스닝 중입니다...")
+
+    # 데이터 수신
+    while True:
+        data, addr = sock.recvfrom(1024)  # 버퍼 크기 1024
+        print(f"받은 데이터: {data.decode('utf-8')} 주소: {addr}")
