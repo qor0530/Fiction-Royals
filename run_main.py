8c22d3889bb88f193efd50892e951dab7ee3689e
MinChoi0129 <wjdchs0129@gnu.ac.kr>
2024-11-17 00:20:52 +0900

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -13,8 +13,8 @@ def initialize_camera():
     else:
         cap = cv2.VideoCapture(0)
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     cap.set(cv2.CAP_PROP_FPS, 60)
 
     if not cap.isOpened():
